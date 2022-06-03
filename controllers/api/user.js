bc8f9f72f5dadc888e083e569ab650198a92355f
Sajid Jaber <sajidjaber@yahoo.com>
2022-06-02 20:56:54 -0500

    index.j made for api and routes

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const {Op} = require("sequelize")
+const { Op } = require("sequelize")
+
 // CREATE new user
 router.post('/', async (req, res) => {
   try {
