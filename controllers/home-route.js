ae5f31e503697528969aeaba7d19685ef59d1410
Sajid Jaber <sajidjaber@yahoo.com>
2022-06-02 20:46:40 -0500

    Added routes for api and home

---
+++ controllers/home-route.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const { Animal, Category, Story,User } = require('../models');
+const withAuth = require('../utils/auth');
+// TODO: Import the custom middleware
+
+// GET all Animals for homepage
+router.get('/', async (req, res) => {
+  try {
+    const animalData = await Animal.findAll({
+      include: [
+        {
+          model: Category,
+        },
+      ],
+    });
+
+    const animals = animalData.map((pet) =>
+      animals.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      pets,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/animal/:id',withAuth, async (req, res) => {
+  
+    try {
+      const animalData = await Animal.findByPk(req.params.id, {
+        include: [
+          {
+            model: Category,
+          },
+        ],
+      });
+      const animal = animalData.get({ plain: true });
+      res.render('animal', { animal, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
