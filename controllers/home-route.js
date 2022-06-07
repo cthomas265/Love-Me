72e8ff1275e105887510b3bfea605550dbcad6da
Sajid Jaber <sajidjaber@yahoo.com>
2022-06-07 18:55:56 -0500

    login

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -27,6 +27,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 // GET one gallery
 router.get('/animal/:id',withAuth, async (req, res) => {
   
