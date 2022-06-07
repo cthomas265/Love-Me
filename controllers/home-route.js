00671dca19773a467b47c033631d2afcc9a33918
Sajid Jaber <sajidjaber@yahoo.com>
2022-06-07 10:38:18 -0500

    fixed error in session

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 
     res.render('homepage', {
       animals,
-      loggedIn: false,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     console.log(err);
@@ -44,15 +44,6 @@ router.get('/animal/:id',withAuth, async (req, res) => {
       console.log(err);
       res.status(500).json(err);
     }
-});
-
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-  
-  res.render('login');
 });
 
 module.exports = router;
