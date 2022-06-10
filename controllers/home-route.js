eb90dd41d7fce26ab8039cab561c97c91cd70862
msamm <msamm93@gmail.com>
2022-06-09 20:28:57 -0500

    reverted to original homeroutes

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -27,7 +27,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-
 // GET one gallery
 router.get('/animal/:id',withAuth, async (req, res) => {
   
@@ -50,46 +49,32 @@ router.get('/animal/:id',withAuth, async (req, res) => {
 // GET success stories
 router.get('/success', (req, res) => {
   Story.findAll({
-=======
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
-});
-
-router.get('/animal/:id', (req, res) => {
-  Animal.findOne({
-    where: {
-      id: req.params.id
-    },
     attributes: [
       'id',
-      'description',
-      'name',
+      'content',
       'photo',
+      'user_id'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      },
+      {
+        model: Animal,
+        attributes: ['name'],
+        through: AnimalStory,
+        as: 'animal_stories'
+      },
     ]
   })
-<<<<<<< HEAD
     .then(dbStoryData => {
       console.log(dbStoryData);
       const stories = dbStoryData.map(story => story.get({ plain: true }));
       console.log(stories);
-=======
-    .then(dbPostData => {
-      console.log(dbPostData);
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
->>>>>>> 90d5888d474b0bdd99e5793cdb8c3faf327111e6
 
-      const animals = dbPostData.get({ plain: true });
-
-      console.log(animals);
-      res.render('single-animal', {
-        animals,
+      res.render('success', {
+        stories,
         loggedIn: req.session.loggedIn
       });
     })
@@ -99,31 +84,13 @@ router.get('/animal/:id', (req, res) => {
     });
 });
 
-
-// GET success stories
-router.get('/pets', (req, res) => {
-  Animal.findAll({
-    attributes: [
-      'id',
-      'name',
-      'photo',
-      'description'
-    ],
-
-  })
-    .then(dbStoryData => {
-      console.log(dbStoryData);
-      const animals = dbStoryData.map(story => story.get({ plain: true }));
-
-      res.render('pets', {
-        animals,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  
+  res.render('login');
 });
 
 module.exports = router;
