860a90563bee81b3e187510d068e12591920fff3
msamm <msamm93@gmail.com>
2022-06-12 08:56:36 -0500

    new story creation but still has an error

--- public/js/add-story.js
+++ public/js/add-story.js
@@ -4,12 +4,14 @@ async function newFormHandler(event) {
     const newStoryForm = document.querySelector('.new-story-form');
     const content = document.querySelector('input[name="story-content"]').value;
     const photo = document.querySelector('input[name="story-photo"]').value;
+    const animalId = {};
   
     const response = await fetch(`/api/story`, {
       method: 'POST',
       body: JSON.stringify({
         content,
-        photo
+        photo,
+        animalId
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -19,6 +21,7 @@ async function newFormHandler(event) {
     if (response.ok) {
       newStoryForm.reset();
       document.location.replace('/success');
+      return;
     } else {
       alert(response.statusText);
     }
