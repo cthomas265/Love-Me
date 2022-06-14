08d6a0b7a6f3a78b862de6864961d4d1b50d735e
cthomas265 <98835210+cthomas265@users.noreply.github.com>
2022-06-13 19:41:55 -0500

    css

--- seeds/Animalseed.js
+++ seeds/Animalseed.js
@@ -4,21 +4,21 @@ const animaldata = [
     {
         name: 'Nala',
         age: 4,
-        photo: '/Images/image.png',
+        photo: '/Images/nala.jpg',
         description: 'Likes to stomp to the beat',
         category_id: 1
     },
     {
         name: 'Diamond',
         age: 8,
-        photo: '/Images/image.png',
+        photo: '/Images/diamond.jpg',
         description: 'Stripper in former life. LOVES attention!',
         category_id: 1
     },
     {
         name: 'Biscuit',
         age: 1,
-        photo: '/Images/image.png',
+        photo: '/Images/',
         description: 'Calico tortie baby who bakes',
         category_id: 2
     }
